3c47b52d1e4980fda8b9cd9a1fee20c8a76980d1
Arne Vogt <a.vogt@52north.org>
2021-04-21 17:41:13 +0200

    add unit test

---
+++ test/test_tools.py
@@ -0,0 +1,10 @@
+import unittest
+
+from src.tools import add
+
+class Test_Tools(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(5,4), 9)
+
+if __name__ == '__main__':
+    unittest.main()
